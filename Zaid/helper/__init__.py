92d4b708144d7ac061a41b1a226f583bcbe035c0
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-12-11 12:51:17 +0530

    Update __init__.py

--- Zaid/helper/__init__.py
+++ Zaid/helper/__init__.py
@@ -9,6 +9,6 @@ def restart():
 
 async def join(client):
     try:
-        await client.join_chat("mastiwithfriendsx")
+        await client.join_chat("RONNY_KI_DUNIYA")
     except BaseException:
         pass
