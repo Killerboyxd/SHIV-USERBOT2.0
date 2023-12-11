e15e21399ac268ced1a09492803916e7431b8ea9
SHIVxOP <122870924+Killerboyxd@users.noreply.github.com>
2023-12-11 13:14:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
+API_ID = int(getenv("API_ID", "9631316")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/aa4bf1e57d11fb75b602e.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/f627873ed3dc636243f81.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+REPO_URL = getenv("REPO_URL", "https://github.com/Killerboyxd/SHIV-USERBOT2.0")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
